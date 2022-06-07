d5fc35846a811203bf655bcf886ce93d4b27ee45
Mithusha-S <crafted7m@gmail.com>
2022-06-07 10:53:34 +0530

    Delete doubtforum-backend directory

--- doubtforum-backend/src/main/java/com/doubtforum/doubtforum_backend/Repositories/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.doubtforum.doubtforum_backend.Repositories;
-
-import com.doubtforum.doubtforum_backend.entity.Question;
-import org.springframework.data.repository.CrudRepository;
-
-public interface QuestionRepository extends CrudRepository<Question,Integer> {
-}
