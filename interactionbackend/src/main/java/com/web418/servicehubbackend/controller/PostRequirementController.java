cd57fd10582e9c9d64a25e8f224679e1fb99b672
Dinusha Chandrakumaran <73541514+dinusha19@users.noreply.github.com>
2022-06-07 08:45:39 +0530

    Add files via upload
    Added Interaction back end part

---
+++ interactionbackend/src/main/java/com/web418/servicehubbackend/controller/PostRequirementController.java
@@ -0,0 +1,47 @@
+package com.web418.servicehubbackend.controller;
+
+import com.web418.servicehubbackend.entity.PostRequirement;
+import com.web418.servicehubbackend.service.PostRequirementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/postrequirement")
+public class PostRequirementController {
+
+    @Autowired(required = true)
+    private PostRequirementService postRequirementService;
+
+    @PostMapping("/addPostRequirement")
+    public ResponseEntity<String> addCustomer(@RequestBody PostRequirement postRequirement) {
+        PostRequirement postRequirement1;
+        try {
+            postRequirement1 = postRequirementService.savePostRequirement(postRequirement);
+            return new ResponseEntity<String>("Post Requirement Details Added Successfully", HttpStatus.OK);
+
+        } catch (Exception ex) {
+            return new ResponseEntity<String>("Post Requirement Details Not Added", HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    @GetMapping("/getPostRequirements")
+    public ResponseEntity<ArrayList<PostRequirement>> getPostRequirements() {
+
+        ArrayList<PostRequirement> postRequirements = null;
+        try {
+            postRequirements = postRequirementService.getPostRequirements();
+
+            return new ResponseEntity<ArrayList<PostRequirement>>(postRequirements, HttpStatus.OK);
+
+
+        } catch (Exception ex) {
+            return null;
+        }
+
+    }
+}
---
+++ interactionbackend/src/main/java/com/web418/servicehubbackend/service/PostRequirementService.java
@@ -0,0 +1,25 @@
+package com.web418.servicehubbackend.service;
+
+import com.web418.servicehubbackend.Repository.PostRequirementRepository;
+import com.web418.servicehubbackend.entity.PostRequirement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+
+@Service
+public class PostRequirementService {
+
+    @Autowired(required = true)
+    public PostRequirementRepository postRequirementRepository;
+
+    public PostRequirement savePostRequirement(PostRequirement postRequirement) {
+
+        return postRequirementRepository.save(postRequirement);
+    }
+
+    public ArrayList<PostRequirement> getPostRequirements() {
+        return (ArrayList<PostRequirement>) postRequirementRepository.findAll();
+
+
+    }
+}
